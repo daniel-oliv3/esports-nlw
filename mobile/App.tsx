eeab18883d41827b402305ae216825ced929d76d
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-09-23 14:13:55 -0300

    Create folder screens in src and Home index.tsx styles.ts

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,7 +1,22 @@
 import { StatusBar } from 'react-native';
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_900Black,
+ } from '@expo-google-fonts/inter';
 import { Background } from './src/components/Background';
 
 export default function App() {
+
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_900Black,
+  });
+
   return (
     <Background>
       <StatusBar 
@@ -9,6 +24,8 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
+
+      {fontsLoaded ? }
       
     </Background>
   );
