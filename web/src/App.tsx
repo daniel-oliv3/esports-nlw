7d97c37417d4a6103a265095cac85ed5cc974881
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-09-23 09:41:14 -0300

    Button

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -63,12 +63,18 @@ function App() {
       </div>
 
       <div className="pt-1 bg-nlw-gradient self-stretch  mt-8 rounded-lg overflow-hidden">
-        <div className="bg-[#2A2634] px-8 py-6">
-          <strong className="text-2xl text-white font-black block">Não encontrou seu duo?</strong>
-          <span className="text-zinc-400 block">Publique um anúncio para encontrar novos players!</span>
+        <div className="bg-[#2A2634] px-8 py-6 flex justify-between items-center">
+          <div className="">
+            <strong className="text-2xl text-white font-black block">Não encontrou seu duo?</strong>
+            <span className="text-zinc-400 block">Publique um anúncio para encontrar novos players!</span>
+          </div>
+
+          <button className="py-3 px-4 bg-violet-500 hover:bg-violet-700 text-white rounded">
+            Publicar anúncio
+          </button>
+
         </div>
       </div>
-
     </div>
   )
 }
