98a5a2627f15dc1e907b32b7e68b1050480acfa2
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-09-23 08:45:55 -0300

    Update

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -14,6 +14,11 @@ function App() {
       <div className="grid grid-cols-6 gap-6 mt-16">
         <a href="" className="relative">
           <img src="/imagem1.png" alt="" />
+
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block">League of Legends</strong>
+            <span className="text-zinc-300 text-sm block">4 anúncios</span>
+          </div>
         </a>
         <a href="" className="relative">
           <img src="/imagem2.png" alt="" />
