de7d2871759a6858dd13bfcf9195dad65ff92fc9
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-09-23 14:31:57 -0300

    Create component Loading and index.tsx and styles.ts

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -6,6 +6,8 @@ import {
   Inter_700Bold,
   Inter_900Black,
  } from '@expo-google-fonts/inter';
+
+import { Home } from './src/screens/Home'; 
 import { Background } from './src/components/Background';
 
 export default function App() {
@@ -25,7 +27,7 @@ export default function App() {
         translucent
       />
 
-      {fontsLoaded ? }
+      {fontsLoaded ? <Home /> : }
       
     </Background>
   );
