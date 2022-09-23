806b15b87146b0a96f94f81c33702cf3d93b300e
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-09-23 08:58:45 -0300

    Update

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -12,7 +12,7 @@ function App() {
       </h1>
 
       <div className="grid grid-cols-6 gap-6 mt-16">
-        <a href="" className="relative">
+        <a href="" className="relative rounded-lg overflow-hidden">
           <img src="/imagem1.png" alt="" />
 
           <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
@@ -22,18 +22,43 @@ function App() {
         </a>
         <a href="" className="relative">
           <img src="/imagem2.png" alt="" />
+
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block">Dota 2</strong>
+            <span className="text-zinc-300 text-sm block">9 anúncios</span>
+          </div>
         </a>
         <a href="" className="relative">
           <img src="/imagem3.png" alt="" />
+
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block">Counter Striker</strong>
+            <span className="text-zinc-300 text-sm block">6 anúncios</span>
+          </div>
         </a>
         <a href="" className="relative">
           <img src="/imagem4.png" alt="" />
+
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block">World of Warcraft</strong>
+            <span className="text-zinc-300 text-sm block">2 anúncios</span>
+          </div>
         </a>
         <a href="" className="relative">
           <img src="/imagem5.png" alt="" />
+
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block">Apex Legends</strong>
+            <span className="text-zinc-300 text-sm block">3 anúncios</span>
+          </div>
         </a>
         <a href="" className="relative">
           <img src="/imagem6.png" alt="" />
+
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block">Fortinite</strong>
+            <span className="text-zinc-300 text-sm block">1 anúncios</span>
+          </div>
         </a>
       </div>
 
