ccc2913529babd95e731ccbb769ac75ce020ccaf
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-09-23 13:25:53 -0300

    Background mobile

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,11 +1,11 @@
-import { View } from 'react-native';
 
+import { Background } from './src/components/Background';
 
 export default function App() {
   return (
-    <View>
+    <Background>
       
-    </View>
+    </Background>
   );
 }
 
