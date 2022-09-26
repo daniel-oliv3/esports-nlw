98ffc994dd3731debdea316bbb7f1f3382f2178f
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-09-26 08:56:52 -0300

    Create logo mobile

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,10 +1,17 @@
-import { View } from 'react-native';
+import React from 'react';
+import { View, Image } from 'react-native';
 
+
+import logoImg from '../../assets/logo-nlw-esports.png';
 import { styles } from './styles';
 
 export function Home() {
   return (
     <View style={styles.container}>
+      <Image 
+        source={logoImg}
+        style={styles.logo}      
+      />
 
     </View>
   );
