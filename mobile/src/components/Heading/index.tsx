03177514a004a31ef8269d395647b40db25773b3
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-09-26 09:02:29 -0300

    Create folder Heading in components and index.tsx e styles.ts

---
+++ mobile/src/components/Heading/index.tsx
@@ -0,0 +1,11 @@
+import { View } from 'react-native';
+
+import { styles } from './styles';
+
+export function Heading() {
+  return (
+    <View style={styles.container}>
+        
+    </View>
+  );
+}
