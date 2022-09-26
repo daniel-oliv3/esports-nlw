fc12b049a5a4dab2a96ac55e097f022fbda25f93
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-09-26 10:12:54 -0300

    Update

--- mobile/src/components/GameCard/index.tsx
+++ mobile/src/components/GameCard/index.tsx
@@ -1,12 +1,26 @@
-import React from 'react';
-import { View } from 'react-native';
+import { TouchableOpacity, ImageBackground, ImageSourcePropType } from 'react-native';
 
 import { styles } from './styles';
 
-export function GameCard() {
-  return (
-    <View style={styles.container}>
 
-    </View>
+export interface GameCardProps {
+  id: string;
+  name: string;
+  ads: string;
+  cover: ImageSourcePropType;
+}
+
+interface Props {
+  data: GameCardProps;
+}
+
+export function GameCard({ data }: Props) {
+  return (
+    <TouchableOpacity style={styles.container}>
+      <ImageBackground
+        style={styles.cover}
+        source={data.cover}
+      />
+    </TouchableOpacity>
   );
 }
