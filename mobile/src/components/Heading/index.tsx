144525b5b26227d2e7b0abb3b594ecf511700cdc
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-09-26 09:19:49 -0300

    TTitle and Subtitle

--- mobile/src/components/Heading/index.tsx
+++ mobile/src/components/Heading/index.tsx
@@ -1,11 +1,22 @@
-import { View } from 'react-native';
+import { View, Text, ViewProps } from 'react-native';
 
 import { styles } from './styles';
 
-export function Heading() {
+interface Props extends ViewProps {
+  title: string;
+  subtitle: string;
+}
+
+export function Heading({ title, subtitle, ...rest }: Props) {
   return (
-    <View style={styles.container}>
-        
+    <View style={styles.container} {...rest}>
+        <Text style={styles.title}>
+          {title}
+        </Text>
+
+        <Text style={styles.subtitle}>
+          {subtitle}
+        </Text>
     </View>
   );
 }
--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -3,6 +3,7 @@ import { View, Image } from 'react-native';
 
 
 import logoImg from '../../assets/logo-nlw-esports.png';
+import { Heading } from '../../components/Heading';
 import { styles } from './styles';
 
 export function Home() {
@@ -11,6 +12,11 @@ export function Home() {
       <Image 
         source={logoImg}
         style={styles.logo}      
+      />
+
+      <Heading
+        title="Encontre seu duo!"
+        subtitle="Selecione o game que deseja jogar..."
       />
 
     </View>
