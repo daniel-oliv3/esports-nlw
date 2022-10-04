730a4b001a28643c91b2eca8f85f2a908cec4334
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-10-04 15:36:52 -0300

    Exemplo de useState

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -3,10 +3,18 @@ import { GameBanner } from './components/GameBanner';
 import './styles/main.css';
 import logoImg from '../src/assets/Logo.png';
 import { CreateAdBanner } from './components/CreateAdBanner';
+import { useState } from 'react';
 
 
 function App() {
   
+  const [hasUserClickedOnButton, setHasUserClickedOnButton] = useState(false);
+
+  function handleButtonClick(){
+    setHasUserClickedOnButton(true);
+  }
+
+
   return (
     <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
       <img src={logoImg} alt="" />
@@ -15,14 +23,18 @@ function App() {
         Seu <span className='text-transparent bg-nlw-gradient bg-clip-text'>duo</span> está aqui.
       </h1>
 
+      <button onClick={handleButtonClick}>Botão</button>
+
+      { hasUserClickedOnButton ? 'Usuario clicou' : null}
+
       <div className="grid grid-cols-6 gap-6 mt-16">
         
         <GameBanner bannerUrl="../public/imagem1.png" title="League of Legends" adsCount={5} />
         <GameBanner bannerUrl="../public/imagem2.png" title="Dota 2" adsCount={3} />
         <GameBanner bannerUrl="../public/imagem3.png" title="Counter Striker" adsCount={1} />
         <GameBanner bannerUrl="../public/imagem4.png" title="World of Warcraft" adsCount={9} />
-        <GameBanner bannerUrl="../public/imagem5.png" title="League of Legends" adsCount={7} />
-        <GameBanner bannerUrl="../public/imagem6.png" title="League of Legends" adsCount={4} />
+        <GameBanner bannerUrl="../public/imagem5.png" title="Apex Legends" adsCount={7} />
+        <GameBanner bannerUrl="../public/imagem6.png" title="Fortnite" adsCount={4} />
 
       </div>
 
